70a0a817dc49a9728ef5cf1cdc8cdf24214702c0
Yaniv Blumenfeld <yanivblm6@gmail.com>
2020-08-13 21:44:23 -0700

    fixed cosine annealin fixed length

--- mel2spec/train.py
+++ mel2spec/train.py
@@ -132,7 +132,7 @@ class Trainer:
         self.__init_device(hp.device)
 
         if  hp.optimizer == "novograd":
-            self.scheduler = lr_scheduler.CosineAnnealingLR(self.optimizer, 11907*3 ,1e-4)
+            self.scheduler = lr_scheduler.CosineAnnealingLR(self.optimizer, 744*3 ,1e-4)
         else:
             self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optimizer, **hp.scheduler)
 
